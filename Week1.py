2e854abe11f9399435ffc064c92102616b4cd3a6
Marcus Herstik <marcus@herstik.com>
2024-07-15 20:01:13 +1000

    Create Week1.py

---
+++ Week1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 15:35:30 2024
+
+@author: marcus
+"""
+
+# ask someones name
+
+name = input("What is your name? ")
+
+# name = "Marcus"
+
+print(name)
+
+print(type(name))
+
+# num = 42
+
+num = input("What is your favourite number? ")
+
+num = int(num)
+
+print(num)
+print(type(num))
+
+# # num = 42.0
+# print(num)
+# print(type(num))
+
+# print(num - 4)
+
+print(name + "' favourite number is " + str(num) )
+# print(name,"' favourite number is", num)
+
+print("The next number up is " + str(num + 1))
+
+### 
+# ask the year of birth
+# calcute your age... this year
+
+
+
