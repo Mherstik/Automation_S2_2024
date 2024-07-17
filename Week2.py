69af9cdfc37bbcfdbd98a68b715a7cb14bfe8a00
Marcus Herstik <marcus@herstik.com>
2024-07-17 17:23:01 +1000

    Create Week2.py

---
+++ Week2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 15:44:13 2024
+
+@author: marcus
+"""
+
+currentYear = 2024
+print(currentYear)
+print(type(currentYear))
+
+name = input("What is your name? ")
+
+print("Hello", name)
+# ask the year of birth
+
+year = input("What year were you born? ")
+
+### If this, then that... 
+## If not this... maybe that??
+
+# if year.isdigit():
+#     year = int(year)
+if year.isdigit():
+    print("You gave me a number.")
+    if int(year) > 2024:
+        print("That's not a valid year 2")
+    else:
+        pass
+elif year.isdigit():
+    year = int(year)
+else:
+    print("You didn't give a valid year")
+
+
+# calcute your age... this year
+
+
+
+# if the persons age is above 18 
+# say they can go to the races
+
+# if person below 13 years old - 
+# must be with a responsible adult
+
+# otherwise say you need to get 
+# the parents permission
