faf226f09158445d464e182f6c648bdb92974eb0
Marcus Herstik <marcus@herstik.com>
2024-07-17 18:07:16 +1000

    Update Week2.py

--- Week2.py
+++ Week2.py
@@ -10,6 +10,7 @@ currentYear = 2024
 # print(currentYear)
 # print(type(currentYear))
 
+loops= 0
 name = input("What is your name? ")
 
 print("Hello", name)
@@ -22,20 +23,17 @@ year = input("What year were you born? ")
 
 # if year.isdigit():
 #     year = int(year)
-if year.isdigit():
-    print("You gave me a number.")
-    if int(year) > 2024 or int(year) < 1900:
-        print("Stop being a goofball")        
-    else:
-        pass
-elif year.isdigit():
-    year = int(year)
+
+while year.isdigit() == False:
+    year = input("What year were you born? ")
+
+year = int(year)
+if year > 2024:
+    print("THis")
+elif year < 1900:
+    print("That")
 else:
-    print("You didn't give a valid year")
-
-
-while 
-
+    print("Come on... be reasonable")
 
 # calcute your age... this year
 
