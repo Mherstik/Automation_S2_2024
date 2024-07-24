6bcce65fcd3e6d8aafd5ebe0d4bb3980827849e8
Marcus Herstik <marcus@herstik.com>
2024-07-24 15:07:44 +1000

    Create Week3.py

---
+++ Week3.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 14:20:56 2024
+
+@author: marcus
+"""
+
+# a = 5
+# b = 2
+
+# print(a/b)
+# print(a%b)  # remainder
+# print(a//b) # modulus = whole times it fits
+# print(a**b)
+# print(a*b)
+
+# print((int(21** 0.5 )+1))
+
+###
+# Check number from 2 --> int(num ** 0.5 +1)
+#    if number remainder from each number is 0 
+#        then the number is not a prime
+
+
+
+userInput = int(input("Choose a number: "))
+# if a == 2:
+#     print("It's TRUE")
+
+def isPrime(num):
+    # check if number less than 2
+    # and give back a false
+    
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+if isPrime(userInput) == True:
+    print("It's a prime")
+else:
+    print("Not a prime")
+    
