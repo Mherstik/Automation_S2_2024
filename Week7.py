af1186f318a2c7683604d06e4f0d54e7b5ed7814
Marcus Herstik <marcus@herstik.com>
2024-08-21 15:22:28 +1000

    Update Week7.py

--- Week7.py
+++ Week7.py
@@ -6,18 +6,42 @@ Created on Wed Aug 21 14:11:55 2024
 @author: marcus
 """
 
-# Open a file
-filename = "File1.txt"
-
-# File_object = open(r"File_Name", "Access_Mode")
-
-# f = open(r"C:\Documents\temp\filename.txt")
-f = open(filename, "r")
 
 
-# Read contents
-print(f.read())
+# CREATE A FILE
+# Option 1
+filename = "file2.txt"
+
+# open file for writing
+file = open(filename, 'w')
+
+# write content
+file.write("WHAT!?")
+
+# close the file
+file.close()
+
+# Option 2
+with open(filename, 'w') as file:
+    file.write("This is content from with")
+    
+
+####
+
+# # Open a file
+# filename = "File1.txt"
+
+# # File_object = open(r"File_Name", "Access_Mode")
+
+# # f = open(r"C:\Documents\temp\filename.txt")
+# f = open(filename, "r")
+
+# # with open("file1.txt", w) as f1:
+# #     f1.write
+
+# # Read contents
+# print(f.read())
 
 
-# Close file
-f.close()
+# # Close file
+# f.close()
