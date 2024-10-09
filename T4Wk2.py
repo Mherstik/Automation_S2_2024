9884ea940e6a5cf5e42bdeabeb8d60fbcc41a387
Marcus Herstik <marcus@herstik.com>
2024-10-09 17:33:10 +1000

    Update T4Wk2.py

--- T4Wk2.py
+++ T4Wk2.py
@@ -8,8 +8,6 @@ Created on Wed Oct  9 14:38:36 2024
 
 from time import time, ctime, gmtime
 
-pcTime = ''
-
 def get_time():
     t = time()
     return (ctime(t)) #, gmtime(t))
@@ -29,6 +27,8 @@ print(secondTime)
 
 import os
 print("Using OS")
+os_name = os.name
+os_uname = os.uname()
 print(os.name)
 print(os.uname())
 
@@ -50,7 +50,47 @@ def get_IPaddress():
         print(os.system('ip a'))
     else:
         print(os.system('ipconfig'))
-
+# this has issues as it's using the OS commands
 get_IPaddress()
-import socket
+
+### open file to write to it
+#  check to see if file exists...?
+
+# open for writing
+with open("testfile.csv", 'w') as f:
+    f.write(str(pcTime + "," + osType + "," + os_name))
+
+with open('testfile.csv', 'r') as f:
+    print(f.read())
+    
+
+
+
+## stolen from https://www.geeksforgeeks.org/get-your-system-information-using-python-script/
+import platform
+
+my_system = platform.uname()
+
+print(f"System: {my_system.system}")
+print(f"Node Name: {my_system.node}")
+print(f"Release: {my_system.release}")
+print(f"Version: {my_system.version}")
+print(f"Machine: {my_system.machine}")
+print(f"Processor: {my_system.processor}")
+
+
+
+
+# import socket
+# local_name = socket.gethostname()
+# print(local_name)
+# # this tries to connect to Google DNS
+# # and tells what IP did the connection
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.connect(("8.8.8.8", 80))
+# # get the local IP from the socket connection
+# local_ip = s.getsockname()
+# print(local_ip)
+# # only get the first index from the list
+# print("the IP that connected is from",s.getsockname()[0])
 
