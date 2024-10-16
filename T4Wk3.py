02dcc6ab7a98b75ac769ad8f502b6835d79ff327
Marcus Herstik <marcus@herstik.com>
2024-10-16 15:50:17 +1000

    Update T4Wk3.py

--- T4Wk3.py
+++ T4Wk3.py
@@ -6,27 +6,63 @@ Created on Wed Oct 16 15:15:11 2024
 @author: marcus
 """
 from time import time, ctime, gmtime
+import os
+import sys
 
+# Get time
 def get_time():
     t = time()
     return (ctime(t)) #, gmtime(t))
 
 pcTime = get_time()
 
-import os
+# Get OS name
 os_name = os.name
 
-
-import sys
+# Get OS type
 osType = sys.platform
 
 ### open file to write to it
 #  check to see if file exists...?
 
-# open for writing
-with open("testfile.csv", 'w') as f:
-    f.write(str(pcTime + "," + osType + "," + os_name))
+# Time, ostype, osname
 
-with open('testfile.csv', 'r') as f:
-    print(f.read())
+data = [str(pcTime + "," + osType + "," + os_name)]
+print(data)
+
+## pause
+print("let's pause")
+userInput = input()
+
+# open for writing
+# with open("testfile.csv", 'a') as f:
+#     f.write(str(pcTime + "," + osType + "," + os_name))
+
+# print("read as text file\n")
+# with open('testfile.csv', 'r') as f:
+#     print(f.read())
     
+print("read as CSV file\n")
+import csv
+# exampleFile = open('testfile.csv')
+# exampleReader = csv.reader(exampleFile)
+# exampleData = list(exampleReader)
+# print(exampleData)
+
+### NICER OPENING
+def readCSVFile():
+    with open('testfile.csv', mode='r') as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            print(lines)
+# readCSVFile()
+
+with open('testfile.csv', mode='a') as file:
+    writer = csv.writer(file)
+    writer.writerow(data)
+
+    
+
+readCSVFile()
+
+
